8f3fc086d5a7206147c52bb25aa8687d4a5820e9
Semanuilko <for-sem@yandex.ru>
2018-07-10 19:28:02 +0300

    add mask  param

--- controllers/actions/not_found/index.php
+++ controllers/actions/not_found/index.php
@@ -1,8 +1,3 @@
 <?php
-
-//PageController::render("404");
-
-dump(get_declared_classes());
-
 PageController::render("404");
 ?>
--- controllers/page_controller.php
+++ controllers/page_controller.php
@@ -1,16 +1,16 @@
 <?
 
 class PageController {
-    // static function render($page_name, $params = []) {
+    static function render($page_name, $params = []) {
 
-    //     // foreach ($params as $key => $value) {
-    //     //     $$key = $value;
-    //     // }
+        foreach ($params as $key => $value) {
+            $$key = $value;
+        }
 
-    //     // require_once (__DIR__ . "/../view/header.php");        
-    //     // require_once (__DIR__ . "/../view/pages/" . $page_name . ".php");
-    //     // require_once (__DIR__ . "/../view/footer.php");
-    // }
+        require_once (__DIR__ . "/../view/header.php");        
+        require_once (__DIR__ . "/../view/pages/" . $page_name . ".php");
+        require_once (__DIR__ . "/../view/footer.php");
+    }
 }
 
 ?>
